1fc2b4dd31946ad2b478cc92acbe76ff5e6cb1e9
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2024-05-30 14:44:39 +0530

    feat: add UUID and rename JSON to JSONObject as it is conflicting with window.JSON

--- src/UUID.ts
+++ src/UUID.ts
@@ -1,1 +1,7 @@
-export type UUID = string;
+/**
+ * Represents a UUID (Universally Unique Identifier) with enforced format for version 4 UUIDs.
+ *
+ * Note: TypeScript does not currently support regex-based types. The current definition ensures that parts of the UUID are strings, but it does not validate their length.
+ * See: https://github.com/microsoft/TypeScript/issues/6579
+ */
+export type UUID = `${string}-${string}-4${string}-${string}`;
